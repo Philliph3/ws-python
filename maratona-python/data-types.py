f469329fbe68280e031007798a838f0da80d7841
Jorge Macedo <jorgefilipedasilva@gmail.com>
2023-04-29 20:16:04 -0300

    Create data-types.py

---
+++ maratona-python/data-types.py
@@ -0,0 +1,27 @@
+year = 2022
+age = 22
+
+print(year - age)
+
+nada = None
+texto = "That's my life"
+moeda = 3.50
+status = True
+
+print(texto, type(texto))
+print(age, type(age))
+print(moeda, type(moeda))
+print(status, type(status))
+print(nada, type(nada))
+
+# x = int(input("Please enter an integer: "))
+
+# if x < 0:
+#     x = 0
+#     print('Negative changed to zero')
+# elif x == 0:
+#     print('Zero')
+# elif x == 1:
+#     print('Single')
+# else:
+#     print('More')
