7432e1bb9b4af412a90ef32ee34851e882bc4c3a
Jorge Macedo <jorgefilipedasilva@gmail.com>
2023-05-30 08:09:19 -0300

    comentarios add

--- cpf-generator/main.py
+++ cpf-generator/main.py
@@ -1,6 +1,20 @@
 ### Copiar projeto em C
 ### Criar condicional para verificar qual estado o CPF gerado pertence
-#
+#           
+#    PARA VERIFICAR O ESTADO DE ONDE O CPF PERTENCE BASTA:
+#    1) CPF: abc.def.ghi-jk verifica-se o numero na posição (i)
+#    2) Exemplo: Nº:  003.939.708-41 = Código 8, corresponde ao Estado de São Paulo.
+#    Código 0:  Rio Grande do Sul
+#    Código 1:  Distrito Federal – Goiás – Mato Grosso – Mato Grosso do Sul – Tocantins
+#    Código 2:  Pará – Amazonas – Acre – Amapá – Rondônia – Roraima
+#    Código 3:  Ceará – Maranhão – Piauí
+#    Código 4:  Pernambuco – Rio Grande do Norte – Paraíba – Alagoas
+#    Código 5:  Bahia – Sergipe
+#    Código 6:  Minas Gerais
+#    Código 7:  Rio de Janeiro – Espírito Santo
+#    Código 8:  São Paulo
+#    Código 9: Paraná – Santa Catarina
+#            
 #
 #
 #
