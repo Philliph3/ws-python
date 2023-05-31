b1e0308734d1e519c017bf581da2741982657418
Philliph3 <jomacedo@proton.me>
2023-05-31 14:08:49 -0300

    comment adjusted

--- basico-a-derivada/aula-03.py
+++ basico-a-derivada/aula-03.py
@@ -1,8 +1,8 @@
 ## Python is case-sensitive 
 x=5.1 ## letter x in lowercase
 X=5.2 ## letter X in uppercase
-y=7.0 ## letter x in lowercase
-Y=7.5 ## letter X in uppercase
+y=7.0 ## letter y in lowercase
+Y=7.5 ## letter Y in uppercase
 
 print("Variable x lowercase: ", x)
 print("Variable X in uppercase: ", X)
