53afe8155a39c754056deb299f91d33350488dec
Philliph3 <jomacedo@proton.me>
2023-05-31 14:07:09 -0300

    add other example in code

--- basico-a-derivada/aula-03.py
+++ basico-a-derivada/aula-03.py
@@ -1,5 +1,11 @@
 ## Python is case-sensitive 
 x=5.1 ## letter x in lowercase
 X=5.2 ## letter X in uppercase
+y=7.0 ## letter x in lowercase
+Y=7.5 ## letter X in uppercase
+
 print("Variable x lowercase: ", x)
 print("Variable X in uppercase: ", X)
+
+print("Variable y lowercase: ", y)
+print("Variable Y in uppercase: ", Y)
