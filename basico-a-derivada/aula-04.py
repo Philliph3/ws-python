9e57386c0edc4ca86dbb9f5eb5c485eb670b9518
Jorge Macedo <jorgefilipedasilva@gmail.com>
2023-05-31 12:39:35 -0300

    year example add

--- basico-a-derivada/aula-04.py
+++ basico-a-derivada/aula-04.py
@@ -1,8 +1,12 @@
 age = input("Insert your age: ")
 print(type(age)) ## The type is String, need convert to int type
 
+year = input("Insert year you born: ")
+print(type(year)) ## The type is String, need convert to int type
+
 pi = input("Insert the pi value: ")
 print(type(pi)) ## The type is String, need convert to int type
+
 
 
 
@@ -11,6 +15,9 @@ print("\n")
 ## SOLUTION:
 age = int(input("What is your age? "))
 print(type(age))
+
+year = int(input("Insert year you born: "))
+print(type(year)) ## The type is String, need convert to int type
 
 pi = float(input("Insert the pi value: "))
 print(type(pi))
