1989ad792e91615c820ba54cdb6d91cd96856421
Philliph3 <jomacedo@proton.me>
2023-05-31 12:29:56 -0300

    Course files add

---
+++ basico-a-derivada/aula-04.py
@@ -0,0 +1,16 @@
+age = input("Insert your age: ")
+print(type(age)) ## The type is String, need convert to int type
+
+pi = input("Insert the pi value: ")
+print(type(pi)) ## The type is String, need convert to int type
+
+
+
+print("\n")
+
+## SOLUTION:
+age = int(input("What is your age? "))
+print(type(age))
+
+pi = float(input("Insert the pi value: "))
+print(type(pi))
