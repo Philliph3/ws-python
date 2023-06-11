c2d8d0d0ea024bb1d92d8b756d054a7e7029a047
Jorge Macedo <jorgefilipedasilva@gmail.com>
2023-06-10 21:56:33 -0300

    name chnged

--- IFRJ/lista-A/calculoGasolina.py
+++
@@ -1,4 +0,0 @@
-litroGasolina=5.46 # average price of gasoline in Brazil 2023
-valorPago=float(input("Informe o valor pago:"))
-litrosAbastecidos = (valorPago/litroGasolina)
-print("litrosAbastecidos: " + str(litrosAbastecidos))
