09856f43ee621e94ce103f545869653c6cb8237f
Jorge Macedo <jorgefilipedasilva@gmail.com>
2023-06-10 21:56:22 -0300

    name chnged

--- IFRJ/lista-A/calculoDiasDeVida.py
+++
@@ -1,9 +0,0 @@
-#from datetime import date
-
-name = input("Informe seu nome:")
-age = int(input("Informe sua idade:"))
-#atual = date.today()
-year = 365
-daysOfLife=(age*year)
-#print(daysOfLife)
-print(f"{name}, Have You Already Lived {daysOfLife} Days !")
