87c95fc34e296fc9ce830ca95e45282927a893e1
Jorge Macedo <jorgefilipedasilva@gmail.com>
2023-06-10 21:56:29 -0300

    name chnged

--- IFRJ/lista-A/calculoRestaurante.py
+++
@@ -1,4 +0,0 @@
-priceMealWeight=12
-clientPlateWeght=float(input("Enter the weight of the customer's dish:"))
-totalToPay=(clientPlateWeght*priceMealWeight)
-print("The total to pay is:",totalToPay)
